175ba2c3fbbc192ac3bf2d5925fa7a28e70ff75d
MicoFeb <sutanmico@gmail.com>
2023-10-31 21:20:16 +0700

    cupdate✨ Table Component

--- src/components/OccupancyCard.vue
+++ src/components/OccupancyCard.vue
@@ -1,29 +1,55 @@
 <template>
-  <CardComponent></CardComponent>
+  <div class="card tw-bg-secondary tw-text-black tw-rounded-2xl">
+    <h1 class="tw-text-center tw-font-bold tw-text-xs tw-p-1">{{ title }}</h1>
+    <table class="tw-w-full">
+      <thead
+        class="tw-border-t-2 tw-border-b-2 tw-border-black tw-text-[10px] tw-font-light"
+      >
+        <tr>
+          <td align="center" class="tw-border-r-2 tw-border-black">Room</td>
+          <td align="center">Person</td>
+        </tr>
+      </thead>
+      <tbody class="tw-text-4xl tw-font-extrabold">
+        <tr>
+          <td align="center" class="tw-border-r-2 tw-border-black">
+            {{ roomTotal }}
+          </td>
+          <td align="center">{{ personTotal }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 <script>
 import { defineComponent } from 'vue';
-import CardComponent from './CardComponent.vue';
 
 export default defineComponent({
   name: 'OccupancyCard',
-  components: { CardComponent },
   props: {
     title: {
       type: String,
+      default: 'loading',
       required: true,
     },
 
     roomTotal: {
       type: String,
-      default: '',
+      default: '0',
+      required: true,
     },
 
     personTotal: {
       type: String,
-      default: '#',
+      default: '0',
+      required: true,
     },
   },
 });
 </script>
-<style></style>
+<style>
+.card {
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  width: 146px;
+}
+</style>
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tw-mt-10">
+  <div class="tableComp tw-mt-10">
     <q-table
       :rows="rows"
       :columns="columns"
@@ -27,26 +27,26 @@ export default {
 </script>
 
 <style>
-.q-table th {
+.tableComp .q-table th {
   @apply tw-text-sm tw-border-r-4 tw-border-t-0 tw-border-l-0 tw-border-b-0 tw-border-white;
 }
-.q-table tbody td {
+.tableComp .q-table tbody td {
   font-size: 14px;
   border-right: 4px solid white;
 }
-.q-table tbody tr:nth-child(even) {
+.tableComp .q-table tbody tr:nth-child(even) {
   background-color: #fafafa;
 }
-.q-table__bottom {
+.tableComp .q-table__bottom {
   @apply tw-inline-flex tw-justify-between tw-text-sm tw-border-2 tw-border-b-0 tw-border-l-0 tw-border-r-0 tw-border-[#069550] tw-bg-[#fafafa] tw-relative;
 }
-.q-table__control:nth-child(2) {
+.tableComp .q-table__control:nth-child(2) {
   @apply tw-absolute;
 }
-.q-table__bottom .q-btn .q-icon {
+.tableComp .q-table__bottom .q-btn .q-icon {
   @apply tw-text-3xl tw-text-[#069550];
 }
-.disabled * {
+.tableComp .disabled * {
   @apply tw-opacity-0;
 }
 </style>
