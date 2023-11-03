ab8f5f0b2c791a304f8734796d27808f9ff88ea8
unknown <arsyibudi5@gmail.com>
2023-11-03 08:31:31 +0700

    tambahan 0-0-0

--- src/pages/dashboard/OOOPage.vue
+++ src/pages/dashboard/OOOPage.vue
@@ -1,22 +1,22 @@
 <template>
   <q-page class="container bg-white flex">
-    <div class="container tw-shadow-md tw-m-8 tw-overflow-auto tw-overflow-x-hidden tw-rounded-xl tw-border tw-border-gray-300">
+    <div class="lg:container xl:container tw-shadow-md tw-m-8 tw-rounded-xl tw-border tw-border-gray-300">
       <div class="tw-flex justify-end">
-          <button class="tw-mx-1">
-            <q-icon
-              name="fa-regular fa-square-check"
-              class="tw-text-4xl tw-py-2"
-              style="color: #069550"
-            />
-          </button>
-          <button class="tw-mx-2">
-            <q-icon
-              name="fa-solid fa-print"
-              class="tw-text-4xl tw-px-1"
-              style="color: #069550"
-            />
-          </button>
-        </div>
+        <button class="tw-mx-1">
+          <q-icon
+            name="fa-regular fa-square-check"
+            class="tw-text-4xl tw-py-2"
+            style="color: #069550"
+          />
+        </button>
+        <button class="tw-mx-2">
+          <q-icon
+            name="fa-solid fa-print"
+            class="tw-text-4xl tw-px-1"
+            style="color: #069550"
+          />
+        </button>
+      </div>
       <div class="tw-flex">
         <p class="tw-font-bold tw-text-1xl tw-ml-5 tw-mt-5">Display</p>
         <select class="tw-ml-2 tw-w-24 tw-mt-5 tw-border-separate tw-rounded-md tw-border tw-border-gray-300 tw-font-bold">
@@ -26,20 +26,20 @@
         </select>
       </div>
       <div class="tw-flex tw-justify-between">
-        <div class="tw-flex"> 
-        <p class="tw-font-bold tw-text-1x1 tw-ml-5 tw-mt-5">Sorting</p>
-        <select class="tw-ml-2 tw-mt-5 tw-w-24 tw-rounded-md tw-border tw-border-gray-300 tw-font-bold">
-          <option value="asc">Ascending</option>
-          <option value="desc">Descending</option>
-        </select>
-      </div>
-      <div class="tw-flex">
-        <p class="tw-font-bold tw-text-1x1">From</p>
-        <input type="text" name="from" id="from" class="tw-bg-gray-100 tw-h-6 tw-font-bold tw-rounded-md tw-border tw-border-gray-300 tw-h-5 tw-py-1 tw-mx-2 text-black font-semibold placeholder-gray-400 placeholder-opacity-100 md:placeholder-opacity-0">
-        <p class="tw-font-bold tw-text-1x1">To</p>
-        <input type="text" name="from" id="from" class="tw-bg-gray-100 tw-h-6 tw-font-bold tw-rounded-md tw-border tw-border-gray-300 tw-h-5 tw-py-1 tw-mx-2 text-black font-semibold placeholder-gray-400 placeholder-opacity-100 md:placeholder-opacity-0">
-      </div>
+        <div class="tw-flex">
+          <p class="tw-font-bold tw-text-1x1 tw-ml-5 tw-mt-5">Sorting</p>
+          <select class="tw-ml-2 tw-mt-5 tw-w-24 tw-rounded-md tw-border tw-border-gray-300 tw-font-bold">
+            <option value="asc">Ascending</option>
+            <option value="desc">Descending</option>
+          </select>
         </div>
+        <div class="tw-flex">
+          <p class="tw-font-bold tw-text-1x1">From</p>
+          <input type="text" name="from" id="from" class="tw-bg-gray-100 tw-h-6 tw-font-bold tw-rounded-md tw-border tw-border-gray-300 tw-h-5 tw-py-1 tw-mx-2 text-black font-semibold placeholder-gray-400 placeholder-opacity-100 md:placeholder-opacity-0">
+          <p class="tw-font-bold tw-text-1x1">To</p>
+          <input type="text" name="to" id="to" class="tw-bg-gray-100 tw-h-6 tw-font-bold tw-rounded-md tw-border tw-border-gray-300 tw-h-5 tw-py-1 tw-mx-2 text-black font-semibold placeholder-gray-400 placeholder-opacity-100 md:placeholder-opacity-0">
+        </div>
+      </div>
       <table class="tw-table tw-m-5 tw-shadow-md w-rounded-full">
         <thead>
           <tr>
@@ -65,7 +65,6 @@
     </div>
   </q-page>
 </template>
-
 <script lang="ts">
 export default {
   name: 'OOOPage',
